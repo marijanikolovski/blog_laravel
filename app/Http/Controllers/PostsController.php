c91524997481031f0e6aa12f3720f17845dc8eac
marijanikolovski <m.kuzeljevic@gmail.com>
2022-10-05 22:53:29 +0200

    Create Posts and Single Post

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostsController extends Controller
+{
+    public function index() 
+    {
+        $posts = Post::all();
+
+        return view('blog.index', compact('posts'));
+    }
+
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        return view('blog.show', compact('post'));
+    }
+}
---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @foreach ($posts as $post)
+        <h2><a href="{{ route('blog-index', ['id' => $post->id]) }}">{{ $post->title }}</a></h2>
+        <p>{{ $post->body }}</p>
+    @endforeach
+</body>
+</html>
